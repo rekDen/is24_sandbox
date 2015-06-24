d7b311601a7a1ecdb2a39d1468382938a50818be
Jonatahevoli <denis.berkovich@gmail.com>
2015-06-24 19:42:38 +0200

    yeah

--- README.md
+++ README.md
@@ -1,1 +1,104 @@
-# is24_sandbox
+# is24
+
+## Overview
+
+Simple Ruby Client for Immobilienscout24.
+
+### Installation
+
+```ruby
+gem install is24
+```
+
+### Usage
+
+```ruby
+# create client
+client = Is24::Api.new(
+  token: YOUR_ACCESS_TOKEN,
+  secret: YOUR_TOKEN_SECRET,
+  consumer_key: YOUR_CONSUMER_KEY,
+  consumer_secret: YOUR_CONSUMER_SECRET
+)
+```
+
+Get exposé for object
+
+```ruby
+expose = client.expose(ID)
+
+# complete expose
+puts expose
+
+# eg. address
+puts expose.contactDetails.address
+```
+
+## Authentication
+
+IS24 uses oauth v1.0.
+
+### Step 1: Get a request token
+
+Set up a new client
+
+```ruby
+client = Is24::Api.new(
+  consumer_key: YOUR_CONSUMER_KEY,
+  consumer_secret: YOUR_CONSUMER_SECRET
+)
+```
+
+and request a request_token providing a callback uri to your app
+
+```ruby
+request_token = client.request_token(YOUR_CALLBACK_URI)
+
+## returns
+# {
+#  :oauth_token => OAUTH_TOKEN,
+#  :oauth_token_secret => OAUTH_TOKEN_SECRET,
+#  :redirect_uri => "http://rest.immobilienscout24.de/restapi/security/oauth/confirm_access?oauth_token=OAUTH_TOKEN"
+# }
+```
+
+Redirect your user to the redirect_uri to authorize your request.
+
+### Step 2: Request access token
+
+Once the user has authorized your request you'll receive a get request to your
+callback. Parse the response params and grab your tokens:
+
+```ruby
+tokens = client.request_access_token(
+  oauth_token: OAUTH_TOKEN,
+  oauth_token_secret: OAUTH_TOKEN_SECRET,
+  oauth_verifier: OAUTH_VERIFIER )
+
+## returns
+# {
+#  :oauth_token=> OAUTH_TOKEN,
+#  :oauth_token_secret=> OAUTH_TOKEN_SECRET
+# }
+```
+
+Until the user revokes permission to your app, the access_token is valid for an unlimited time. Store the tokens and reuse them for subsequent calls to restricted resources of the REST API.
+
+## TODO
+
+* Error handling ;)
+
+## Contributing to is24
+ 
+* Check out the latest master to make sure the feature hasn't been implemented or the bug hasn't been fixed yet.
+* Check out the issue tracker to make sure someone already hasn't requested it and/or contributed it.
+* Fork the project.
+* Start a feature/bugfix branch.
+* Commit and push until you are happy with your contribution.
+* Make sure to add tests for it. This is important so I don't break it in a future version unintentionally.
+* Please try not to mess with the Rakefile, version, or history. If you want to have your own version, or is otherwise necessary, that is fine, but please isolate to its own commit so I can cherry-pick around it.
+
+## Copyright
+
+Copyright (c) 2013 Bernd Suenkel. See LICENSE.txt for
+further details.
---
+++ lib/is24.rb
@@ -0,0 +1,227 @@
+# encoding: UTF-8
+
+require 'is24/logger'
+require 'faraday'
+require 'faraday_middleware'
+require 'faraday_middleware/response/mashify'
+require 'cgi'
+
+module Is24
+  class Api
+    include Is24::Logger
+
+    API_ENDPOINT = "http://rest.immobilienscout24.de/restapi/api/search/v1.0/"
+    API_OFFER_ENDPOINT = "http://rest.immobilienscout24.de/restapi/api/offer/v1.0/"
+    API_AUTHORIZATION_ENDPOINT = "http://rest.immobilienscout24.de/restapi/security/"
+
+    # TODO move in separate module
+    MARKETING_TYPES = {
+      "PURCHASE" => "Kauf",
+      "PURCHASE_PER_SQM" => "Kaufpreis/ Quadratmeter",
+      "RENT" => "Miete",
+      "RENT_PER_SQM" => "Mietpreis/ Quadratmeter",
+      "LEASE" => "Leasing",
+      "LEASEHOLD" => "",
+      "BUDGET_RENT" => "",
+      "RENT_AND_BUY" => ""
+    }
+
+    PRICE_INTERVAL_TYPES = {
+      "DAY" => "Tag",
+      "WEEK" => "Woche",
+      "MONTH" => "Monat",
+      "YEAR" => "Jahr",
+      "ONE_TIME_CHARGE" => "einmalig"
+    }
+
+    REAL_ESTATE_TYPES = {
+      "APARTMENT_RENT" => "Wohnung Miete",
+      "APARTMENT_BUY" => "Wohnung Kauf",
+      "HOUSE_RENT" => "Haus Miete",
+      "HOUSE_BUY" => "Haus Kauf",
+      "GARAGE_RENT" => "Garage / Stellplatz Miete",
+      "GARAGE_BUY" => "Garage / Stellplatz Kauf",
+      "LIVING_RENT_SITE" => "Grundstück Wohnen Miete",
+      "LIVING_BUY_SITE" => "Grundstück Wohnen Kauf",
+      "TRADE_SITE" => "Grundstück Gewerbe",
+      "HOUSE_TYPE" => "Typenhäuser",
+      "FLAT_SHARE_ROOM" => "WG-Zimmer",
+      "SENIOR_CARE" => "Altenpflege",
+      "ASSISTED_LIVING" => "Betreutes Wohnen",
+      "OFFICE" => "Büro / Praxis",
+      "INDUSTRY" => "Hallen / Produktion",
+      "STORE" => "Einzelhandel",
+      "GASTRONOMY" => "Gastronomie / Hotel",
+      "SPECIAL_PURPOSE" => "",
+      "INVESTMENT" => "Gewerbeprojekte",
+      "COMPULSORY_AUCTION" => "",
+      "SHORT_TERM_ACCOMMODATION" => ""
+    }
+
+    # transforms, eg.
+    # "SPECIAL_PURPOSE" => ""
+    # to
+    # "search:SpecialPurpose" => ""
+    XSI_SEARCH_TYPES = lambda {
+      return Hash[*REAL_ESTATE_TYPES.map{ |v|
+          [
+            "search:"+v.first.downcase.split("_").map!(&:capitalize).join,
+            v[1]
+          ]
+        }.flatten
+      ]
+    }.()
+
+    def self.format_marketing_type(marketing_type)
+      MARKETING_TYPES[marketing_type] || ""
+    end
+
+    def self.format_price_interval_type(price_interval_type)
+      PRICE_INTERVAL_TYPES[price_interval_type] || ""
+    end
+
+    def initialize( options = {} )
+      logger "Initialized b'nerd IS24 with options #{options}"
+
+      @token = options[:token] || nil
+      @secret = options[:secret] || nil
+      @consumer_secret = options[:consumer_secret] || nil
+      @consumer_key = options[:consumer_key] || nil
+
+      raise "Missing Credentials!" if @consumer_secret.nil? || @consumer_key.nil?
+    end
+
+    def request_token( callback_uri )
+      # TODO error handling
+      response = connection(:authorization, callback_uri).get("oauth/request_token")
+
+      body = response.body.split('&')
+      response = {
+        :oauth_token => CGI::unescape(body[0].split("=")[1]),
+        :oauth_token_secret => CGI::unescape(body[1].split("=")[1]),
+        :redirect_uri => "http://rest.immobilienscout24.de/restapi/security/oauth/confirm_access?#{body[0]}"
+      }
+    end
+
+    def request_access_token( params = {} )
+      # TODO error handling
+      @oauth_verifier = params[:oauth_verifier]
+      @token = params[:oauth_token]
+      @secret = params[:oauth_token_secret]
+
+      response = connection(:authorization).get("oauth/access_token")
+      body = response.body.split('&')
+
+      response = {
+        :oauth_token => body[0].split('=')[1],
+        :oauth_token_secret => CGI::unescape(body[1].split('=')[1]),
+      }
+
+      # set credentials in client
+      @token = response[:oauth_token]
+      @token_secret = response[:oauth_token_secret]
+
+      # return access token and secret
+      response
+    end
+
+    def search(options)
+      defaults = {
+        :channel => "hp",
+        :realestatetype => ["housebuy"],
+        :geocodes => 1276,
+        :username => "me"
+      }
+      options = defaults.merge(options)
+      types = options[:realestatetype]
+
+      case types
+        when String
+          types = [types]
+      end
+
+      objects = []
+
+      types.each do |type|
+        options[:realestatetype] = type
+        response = connection.get("search/region", options )
+        if response.status == 200
+          if response.body["resultlist.resultlist"].resultlistEntries[0]['@numberOfHits'] == "0"
+            response.body["resultlist.resultlist"].resultlistEntries[0].resultlistEntries = []
+          end
+          objects.push response.body["resultlist.resultlist"].resultlistEntries[0]
+        end
+      end
+
+      objects
+    end
+
+    def expose(id)
+      response = connection.get("expose/#{id}")
+      response.body["expose.expose"]
+    end
+
+    def perform_query(query)
+      response = connection.get(query)
+      response.body
+    end
+
+    def list_exposes
+      response = connection(:offer).get("user/me/realestate?publishchannel=IS24")
+      response.body
+    end
+
+    def short_list
+      response = connection.get("searcher/me/shortlist/0/entry")
+      response.body["shortlist.shortlistEntries"].first["shortlistEntry"]
+    end
+
+    protected
+
+    def connection(connection_type = :default, callback_uri = nil)
+
+      # set request defaults
+      defaults = {
+        :url => API_ENDPOINT,
+        :headers => {
+          :accept =>  'application/json',
+          :user_agent => 'b\'nerd .media IS24 Ruby Client'}
+      }
+
+      defaults.merge!( {
+        :url => API_AUTHORIZATION_ENDPOINT
+      } ) if connection_type =~ /authorization/i
+
+      defaults.merge!( {
+        :url => API_OFFER_ENDPOINT
+      } ) if connection_type =~ /offer/i
+
+
+      # define oauth credentials
+      oauth = {
+        :consumer_key => @consumer_key,
+        :consumer_secret => @consumer_secret,
+        :token => @token,
+        :token_secret => @secret
+      }
+
+      # merge callback_uri if present
+      oauth.merge!( {
+        :callback => callback_uri
+      } ) if connection_type =~ /authorization/i && callback_uri
+
+      # merge verifier if present
+      oauth.merge!( {
+        :verifier => @oauth_verifier
+      } ) if connection_type =~ /authorization/i && @oauth_verifier
+
+      Faraday::Connection.new( defaults ) do |builder|
+            builder.request :oauth, oauth
+            builder.response :mashify
+            builder.response :json unless connection_type =~ /authorization/i
+            builder.adapter Faraday.default_adapter
+          end
+    end
+
+  end
+end
